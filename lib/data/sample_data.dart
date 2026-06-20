65df80db348bb2985a1184750337379406947a7a
Mohammaddaoud15 <mohammaddaoud9999@gmail.com>
2026-06-20 14:06:12 +0300

    Move API key to .env and add dotenv loading

--- lib/data/sample_data.dart
+++ lib/data/sample_data.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../models/news_article.dart';
 
 Future<List<NewsArticle>> fetchNewsArticles() async {
+  final apiKey = dotenv.env['NEWS_API_KEY'];
   final response = await http.get(Uri.parse(
-    'https://newsapi.org/v2/top-headlines?country=us&apiKey=81273ed5acd2490797e7bcd281f4c347',
+    'https://newsapi.org/v2/top-headlines?country=us&apiKey=$apiKey',
   ));
 
   if (response.statusCode == 200) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'models/news_article.dart';
 import 'data/sample_data.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const NewsApp());
 }
-
 class NewsApp extends StatelessWidget {
   const NewsApp({super.key});
 
